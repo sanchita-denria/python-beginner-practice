989d335f205b41fe616b1f2989ada389377ce7f1
sanchita-denria <sanchitadenria2000@gmail.com>
2026-09-17 11:29:32 +0530

    Update student_data.py
    Add student data analysis

--- student_data.py
+++ student_data.py
@@ -6,17 +6,23 @@ for i in range(0,n):
   marks.append(int(input("ENTER MARKS:::")))
 s=0
 high=marks[0]
+low=marks[0]
 j=0
+c=0
 for i in range(0,n):
   s=s+marks[i]
   if(high<marks[i]):
     high=marks[i]
     j=i
+  if (low > marks[i]):
+    low=marks[i]
+    c=i
 print("SUM=",s)
 print("AVERAGE MARKS=",s/n)
 print("HIGHEST MARKS=",high)
 print("NAME OF STUDENT=",name[j])
-
+print("LOWEST MARKS=",low)
+print("NAME OF STUDENT=",name[c])
 
 
   
