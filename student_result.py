d1c42b6d2ac4ead8af004037aea5c1b437ed4b2b
sanchita-denria <sanchitadenria2000@gmail.com>
2026-09-20 19:26:32 +0530

    Update student_result.py
    Improved student analyser result

--- student_result.py
+++ student_result.py
@@ -10,30 +10,35 @@ for i in range(0,n):
      marks.append(int(input("ENTER MARKS:::")))
 print(name)
 print(marks)
-se=input("ENTER A NAME TO SEARCH:::")
-nse=int(input("ENTER MINIMUM MARKS::::"))
 p=0
-for i in range(0,n):
-      if(se==name[i]):
-          print("NAME=",name[i])
-          print("MARKS=",marks[i])
+def search_students():
+  se=input("ENTER A NAME TO SEARCH:::")
+  nse=int(input("ENTER MINIMUM MARKS::::"))
+  p=0 
+  for i in range(0,n):
+        if(se==name[i]):
+            print("NAME=",name[i])
+            print("MARKS=",marks[i])
+            p=p+1
+  if(p==0):
+       print("NOT FOUND!!!")
+  p=0
+  print("MARKS COVERING MINIMUM       RANGE:::")
+    
+  for i in range(0,n):
+      if nse <= marks[i]:
+          if marks[i] >= 40:
+              print(name[i],"-->",marks[i],"-->PASS")
+          else:
+              print(name[i],"-->",marks[i],"-->FAIL")
           p=p+1
-if(p==0):
-     print("NOT FOUND!!!")
-print("MARKS COVERING MINIMUM RANGE:::")
-for i in range(0,n):
-   if(nse<=marks[i] ):
-       if(marks[i]>=40):
-          print(name[i],"-->",marks[i],"-->PASS")
-       else:
-          print(name[i],"-->",marks[i],"-->FAIL")
-          p=p+1
-print("NO. OF STUDENT  MEETING MINIMUM MARKS:::",p)
+          
+  print("NO. OF STUDENTS MEETING MINIMUM MARKS:::",p)
 def show_grades():
   for i in range(0,n):
     if marks[i] >= 90:
         print(name[i],"-->",marks[i],"-->A+")
-    elif marks[i] >= 80:
+    elif marks[i] >=80:
         print(name[i],"-->",marks[i],"-->A")
     elif marks[i] >= 70:
         print(name[i],"-->",marks[i],"-->B+")
